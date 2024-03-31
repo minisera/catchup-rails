78ea6530c7a2abde9494244d9294bc877fada874
minisera <koki.ohtaka0821@gmail.com>
2024-03-31 01:07:58 +0000

    modify: メンバー登録APIのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :groups
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :groups do
+    post 'members', to: 'members#create'
+  end
 end
