61b4cba3253d4982da4161b81ed177c62d63eb84
minisera <koki.ohtaka0821@gmail.com>
2024-04-01 23:01:06 +0900

    feat: book.modelsのテスト記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_24_054521) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_01_132552) do
+  create_table "books", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "groups", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Bookモデルをnewしたときnilでないこと' do
+    expect(Book.new).not_to eq(nil)
+  end
+
+  describe 'Book#title_with_author' do
+    it 'メソッドが呼び出された時、titleとauthorが結んだ文字が出力されること' do
+      book = Book.new(title: '鉄人シリーズ', author: 'ミミック')
+      expect(book.title_with_author).to eq('鉄人シリーズ - ミミック')
+    end
+  end
 end
